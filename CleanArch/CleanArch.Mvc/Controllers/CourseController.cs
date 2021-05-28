9c8281ac4e634f107840d98d6094396e7a916fab
Nandita R <nanditha@wip.com>
2021-05-28 23:45:15 +0530

    Commit changes in controller to authorize, add images and css

---
+++ CleanArch/CleanArch.Mvc/Controllers/CourseController.cs
@@ -0,0 +1,26 @@
+﻿using CleanArch.Application.Services;
+using CleanArch.Application.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArch.Mvc.Controllers
+{
+    [Authorize]
+    public class CourseController : Controller
+    {
+        private ICourseService _service;
+        public CourseController(ICourseService service)
+        {
+            _service = service;
+        }
+        public IActionResult Index()
+        {
+            CourseViewModel model = _service.GetCourses();
+            return View(model);
+        }
+    }
+}
